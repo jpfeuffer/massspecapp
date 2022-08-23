642c318e3bbddd7e8c8403f676bbe52b1320589c
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2022-08-23 12:42:38 +0200

    removed unused script

--- attach.py
+++
@@ -1,9 +0,0 @@
-from pyopenms import FeatureMap
-
-def say(self, msg):
-    print ('%s says %s' % (self.name, msg))
-
-if __name__ == '__main__':
-    f = FeatureMap()
-    FeatureMap.say = say
-    f.say('Hello')
